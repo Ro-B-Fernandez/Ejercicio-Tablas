6bf71747448b0d49a918304bd9ad896519cd512a
Ro-B-Fernandez <104107565+Ro-B-Fernandez@users.noreply.github.com>
2022-05-23 12:40:37 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// Tablas de Multiplicación
+
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
+
+rotulo1.innerHTML = "La tabla de que número desea"
+rotulo2.innerHTML = "Hasta que número"
+
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+let btnEnv = document.getElementById("btnEnviar");
+
+btnEnv.addEventListener("click", () => {
+  let numTabla: number = Number(dato1.value);
+  let limiteTabla: number = Number(dato2.value);
+  let resultado: number = 0
+  let contador: number = 1
+  for(contador = 1; contador <= limiteTabla; contador ++) {
+    resultado = numTabla * contador;
+    console.log(numTabla + " x " + contador + " = " + resultado);
+  }
+}); 
